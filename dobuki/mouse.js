4e1e5079149a393e32255bf1730276c1de07648a
Jack Le Hamster <jacklehamster@gmail.com>
2017-03-11 18:52:31 -0800

    cc

--- dobuki/mouse.js
+++ dobuki/mouse.js
@@ -4,7 +4,7 @@
  	(factory((global.DOK = global.DOK || {})));
  }(this, (function (core) { 'use strict';
     
-    var spot = {x:0,y:0};
+    var spot = {x:0,y:0}, callbacks = [];
     /**
      *  HEADER
      */   
@@ -37,26 +37,50 @@
         if(e.buttons & 1 || touches) {
             var dx = (touches ? touches[0].pageX : e.pageX) - spot.x;
             var dy = (touches ? touches[0].pageY : e.pageY) - spot.y;
+            for(var i=0;i<callbacks.length;i++) {
+                callbacks[i](dx,dy);
+            }
         }
         e.preventDefault();
-    }    
+    }
+
+    function setOnTouch(func) {
+        deactivateTouch();
+        activateTouch();
+        callbacks.push(func);
+    }
+
+    function activateTouch() {
+        document.addEventListener("mousedown", onDown);
+        document.addEventListener("touchstart", onDown);
+        document.addEventListener("mouseup", onUp);
+        document.addEventListener("touchend", onUp);
+        document.addEventListener("mousemove", onMove);
+        document.addEventListener("touchmove", onMove);
+    }
+
+    function deactivateTouch() {
+        document.removeEventListener("mousedown", onDown);
+        document.removeEventListener("touchstart", onDown);
+        document.removeEventListener("mouseup", onUp);
+        document.removeEventListener("touchend", onUp);
+        document.removeEventListener("mousemove", onMove);
+        document.removeEventListener("touchmove", onMove);
+    }
 
     function destroyEverything() {
+        callbacks = [];
+        deactivateTouch();
     }
    
     /**
      *  PUBLIC DECLARATIONS
      */
+    core.setOnTouch = setOnTouch;
     core.destroyEverything = core.combineMethods(destroyEverything, core.destroyEverything);
 
     /**
      *   PROCESSES
      */
-    document.addEventListener("mousedown", onDown);
-    document.addEventListener("touchstart", onDown);
-    document.addEventListener("mouseup", onUp);
-    document.addEventListener("touchend", onUp);
-    document.addEventListener("mousemove", onMove);
-    document.addEventListener("touchmove", onMove);    
-     
+
  })));
